54fa7b6f6f010e3509e16d48b081f9388dbd5133
Tomasz Leś <szkolenia@tomaszles.pl>
2024-02-22 13:42:30 +0100

    q

--- P06Shop.Shared/Services/AuthService/AuthService.cs
+++ P06Shop.Shared/Services/AuthService/AuthService.cs
@@ -13,20 +13,20 @@ namespace P06Shop.Shared.Services.AuthService
         }
         public async Task<ServiceReponse<bool>> ChangePassword(string newPassword)
         {
-            var result = await _httpClient.PostAsJsonAsync("api/auth/changePassword", newPassword);
+            var result = await _httpClient.PostAsJsonAsync("/api/auth/changePassword", newPassword);
             return await result.Content.ReadFromJsonAsync<ServiceReponse<bool>>();
         }
 
         public async Task<ServiceReponse<string>> Login(UserLoginDto userLoginDto)
         {
-            var result = await _httpClient.PostAsJsonAsync("api/auth/login", userLoginDto);
+            var result = await _httpClient.PostAsJsonAsync("/api/auth/login", userLoginDto);
             var data = await result.Content.ReadFromJsonAsync<ServiceReponse<string>>();
             return data;
         }
 
         public async Task<ServiceReponse<int>> Register(UserRegisterDto userLoginDto)
         {
-            var result = await _httpClient.PostAsJsonAsync("api/auth/register", userLoginDto);
+            var result = await _httpClient.PostAsJsonAsync("/api/auth/register", userLoginDto);
             return await result.Content.ReadFromJsonAsync<ServiceReponse<int>>();
         }
     }
--- P11BlazorWebAssembly.Client/Program.cs
+++ P11BlazorWebAssembly.Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using P06Shop.Shared.Confguration;
+using P06Shop.Shared.Services.AuthService;
 using P06Shop.Shared.Services.ProductService;
 using P11BlazorWebAssembly.Client;
 using P11BlazorWebAssembly.Client.Services;
@@ -29,5 +30,6 @@ builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>
 // wymagna paczka: Microsoft.Extensions.Http
 builder.Services.AddHttpClient<IProductService, ProductService>(client => client.BaseAddress = uriBuilder.Uri);
 builder.Services.Configure<AppSettings>(appSettings);
+builder.Services.AddHttpClient<IAuthService, AuthService>(client => client.BaseAddress = uriBuilder.Uri);
 
 await builder.Build().RunAsync();
